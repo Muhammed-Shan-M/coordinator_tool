9a2cbb8407747fd425e57142fbf9eb69bdb0e632
Muhammed Shan <shanushan73011@gmail.com>
2025-09-18 00:44:34 +0530

    add local storage in redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/Store.ts'
-// import ReactDOM from "react-dom/client"
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from './redux/Store.ts'
 import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,12 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore,combineReducers } from "@reduxjs/toolkit"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import reviewReducer from "./slice/qustions"
 
-export const store = configureStore({
-    reducer: {
-        review: reviewReducer
-    }
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['review']
+}
+
+const rootReducer = combineReducers({
+    review:reviewReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, 
+    }),
+})
+
+export const persistor = persistStore(store)
 
 export type Rootstate = ReturnType<typeof store.getState>
 export type AppDispach = typeof store.dispatch
